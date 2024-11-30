6c27bbfa24627c7dd88404135ca434470c9d5aa1
shandikadav <shandikadavid.14@gmail.com>
2024-12-01 00:44:39 +0700

    Completing task 2: struktur folder

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <div>
+        <h2>Ini Artikel 1</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, nobis!</p>
+    </div>
+    <div>
+        <h2>Ini Artikel 2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit accusantium eius dolor esse assumenda enim aliquid.</p>
+    </div>
+</body>
+</html>
